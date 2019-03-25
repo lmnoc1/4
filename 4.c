2b9552021574fc0e1adaee6ff3d2b729fe17b8b2
mxl900 <mxl900@case.edu>
2019-03-25 00:38:11 -0400

    4

--- 4.c
+++ 4.c
@@ -6,6 +6,7 @@
 #include <pthread.h>
 #include <semaphore.h>  
 #include <sys/wait.h>
+#include <string.h>
 ////////////////////// new
 
 #define N 4
@@ -28,15 +29,17 @@ struct condition {
 
 ////////////////////// new
 void *child(void* id_void);
-void cwait(struct condition *c, char who[]);
-void cpost(struct condition *c, char who[]);
+void cwait(struct condition *c, char who[], char sem[]);
+void cpost(struct condition *c, char who[], char sem[]);
 void monitor_produce(int item, int index);
 void monitor_consume();
 void monitor_enter(char who[]);
 void monitor_exit(char who[]);
+void semprint(sem_t s1, sem_t s2, sem_t s3, sem_t s4, char who[], char what[], char how[]);
 
 char par[] = "Parent";
 char chi[] = "Child";
+char what[] = "initial";
 ////////////////////// new
 
 
@@ -58,9 +61,8 @@ int main(int argc, char *argv[]) {
 	sem_init(&(notfull.sem), 0, 0);
 	sem_init(&(notempty.sem), 0, 0);
 
-	cou++;
-	sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-	printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i 	by %s\n", cou, mut, nex, nfu, nem, "Initial");
+
+	semprint(mutex, next, notfull.sem, notempty.sem, "Initial", "Initial", "Initial"); ///// print
 	////////////////////// new
 
 
@@ -70,7 +72,7 @@ int main(int argc, char *argv[]) {
 	int data[] = {10, 20, 30, 40};
 	int i, in = 0; // Index for writing
 	for(i = 0; i < N; i++) {
-
+		//printf("pppppppppppp %i\n",i);
 		monitor_produce(data[i], in);
 		in = (in + 1) % BUFFER_SIZE;
 
@@ -80,98 +82,94 @@ int main(int argc, char *argv[]) {
 
 void *child(void* id_void) {
 	sleep(2); // Delay child start for 2 sec
+
+	//printf("child awake\n");
 	int *id = id_void;
 	int i;
 	for(i = 0; i < N/2; i++) {
+		//printf("ccccccccccc %i\n",i);
 		monitor_consume(); // CORRECTED
 	}
 	pthread_exit(0);
 }
 
 ////////////////////// Modified
-void cwait(struct condition *c, char who[]) {
+void cwait(struct condition *c, char who[], char sem[]) {
 ////////////////////// Modified
 
 	c->count++;
 	if (next_count > 0) {
 		sem_post(&next);
 
-		////////////////////// new
-		cou++;
-		sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-		printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-		////////////////////// new
+		strcpy(what, "next");////////////////////////////////////////////////////
+		semprint(mutex, next, notfull.sem, notempty.sem, who, what, "increase"); ///// print
 	}
 	else{
 		sem_post(&mutex);
 
-		////////////////////// new
-		cou++;
-		sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-		printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-		////////////////////// new
+		strcpy(what, "mutex");////////////////////////////////////////////////////
+		semprint(mutex, next, notfull.sem, notempty.sem, who, what, "increase"); ///// print
 	}
 	sem_wait(&(c->sem));
 
-	////////////////////// new
-	cou++;
-	sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-	printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-	////////////////////// new
+	strcpy(what, sem);////////////////////////////////////////////////////
+	semprint(mutex, next, notfull.sem, notempty.sem, who, what, "decrease"); ///// print
 
 	c->count--;
 }
 
 ////////////////////// Modified
-void cpost(struct condition *c, char who[]) {
+void cpost(struct condition *c, char who[], char sem[]) {
 ////////////////////// Modified
 
 	if (c->count > 0) {
 		next_count++;
 		sem_post(&(c->sem));
 
-		////////////////////// new
-		cou++;
-		sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-		printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-		////////////////////// new
+
+		strcpy(what, sem);////////////////////////////////////////////////////
+		semprint(mutex, next, notfull.sem, notempty.sem, who, what, "increase"); ///// print
 
 		sem_wait(&next);
 
-		////////////////////// new
-		cou++;
-		sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-		printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-		////////////////////// new
+		strcpy(what, "next");////////////////////////////////////////////////////
+		semprint(mutex, next, notfull.sem, notempty.sem, who, what, "decrease"); ///// print
 
 		next_count--;
 	}
 }
 
 void monitor_produce(int item, int index) {
-
+	//printf("count %i\n", count);
 	monitor_enter(par);
 	if (count == BUFFER_SIZE) {
-		cwait(&notfull, par);
+		//printf("parent enter cwait\n");
+		cwait(&notfull, par, "notfull");
 	}
 	buffer[index] = item;
 	printf("Parent: %d\n", item);
 	count++;
-	cpost(&notempty, par);
+
+
+	cpost(&notempty, par, "notempty");
+	
 	monitor_exit(par);
 
 }
 
 void monitor_consume() {
-
+	//printf("count %i\n", count);
 	monitor_enter(chi);
 	if (count == 0) {
-		cwait(&notempty, chi);
+		printf("child enter cwait\n");
+
+		cwait(&notempty, chi, "notempty");
+
 	}
 	int data = buffer[out];
 	printf("Child: %d\n", data);
 	count--;
-	cpost(&notfull, chi);
+	cpost(&notfull, chi, "notfull");
 	out = (out + 1) % BUFFER_SIZE;
 	monitor_exit(chi);
 
@@ -180,11 +178,8 @@ void monitor_consume() {
 void monitor_enter(char who[]) {
 	sem_wait(&mutex);
 
-	////////////////////// new
-	cou++;
-	sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-	printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-	////////////////////// new
+	strcpy(what, "mutex");////////////////////////////////////////////////////
+	semprint(mutex, next, notfull.sem, notempty.sem, who, what, "decrease"); ///// print
 
 }
 
@@ -192,19 +187,21 @@ void monitor_exit(char who[]) {
 	if (next_count > 0) {
 		sem_post(&next);
 
-		////////////////////// new
-		cou++;
-		sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-		printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-		////////////////////// new
+		strcpy(what, "next");////////////////////////////////////////////////////
+		semprint(mutex, next, notfull.sem, notempty.sem, who, what, "increase"); ///// print
+
 	}
 	else {
 		sem_post(&mutex);
 
-		////////////////////// new
-		cou++;
-		sem_getvalue(&mutex, &mut); sem_getvalue(&next, &nex); sem_getvalue(&(notfull.sem), &nfu); sem_getvalue(&(notempty.sem), &nem);
-		printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s\n", cou, mut, nex, nfu, nem, who);
-		////////////////////// new
+		strcpy(what, "mutex");////////////////////////////////////////////////////
+		semprint(mutex, next, notfull.sem, notempty.sem, who, what, "increase"); ///// print
+		
 	}
+}
+
+void semprint(sem_t s1, sem_t s2, sem_t s3, sem_t s4, char who[], char what[], char how[]) {
+	cou++;
+	sem_getvalue(&s1, &mut); sem_getvalue(&s2, &nex); sem_getvalue(&s3, &nfu); sem_getvalue(&s4, &nem);
+	printf("	(b %i) mutex: %i  next: %i  notfull: %i  notempty: %i	by %s	%s	%s\n", cou, mut, nex, nfu, nem, who, what, how);
 }
